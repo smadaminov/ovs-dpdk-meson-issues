339a0adceeb847cc53772e18ad5e058923ab472b
Sergey Madaminov <madaminovs@vmware.com>
2021-08-03 11:09:11 -0700

    test copy script now also copies options file

--- ovs/copy_files.sh
+++ ovs/copy_files.sh
@@ -5,6 +5,8 @@ find . -type f -name "*\.map\.in" | xargs -I{} cp {} ../ovs-vanilla/{}
 cp lib/dirs.c.in ../ovs-vanilla/lib/
 cp config.h.meson ../ovs-vanilla/
 
+cp meson_options.txt ../ovs-vanilla/
+
 cp build-aux/extract-odp-netlink-h.py ../ovs-vanilla/build-aux/
 cp build-aux/extract-odp-netlink-macros-h.py ../ovs-vanilla/build-aux/
 cp build-aux/copy-file-from-build.py ../ovs-vanilla/build-aux/
